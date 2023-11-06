6d3d7f509e680022cb6863e93c01032032ec33e3
tanimtorikul <torikulswe@gmail.com>
2023-11-06 09:12:01 +0600

    initial server setup done and connected with mongodb

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+// FC4wSbarIfoHIYcW
+
+// DB URI
+
+const uri = "mongodb+srv://handyMate:FC4wSbarIfoHIYcW@cluster0.o2yungw.mongodb.net/handyMate?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Handymate is running");
+});
+
+
+
+
+app.get('/api/v1/services', (req, res) => {
+    res.send('It is working')
+})
+
+app.listen(port, () => {
+  console.log(`Handymate app listening on port ${port}`);
+});
