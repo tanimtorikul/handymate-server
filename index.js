90b2a280e36f1f04d786385dec016440f93d3822
tanimtorikul <torikulswe@gmail.com>
2023-11-07 12:30:46 +0600

    find specific service using get method using id

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require("express");
+const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
+
+// middlewares
+
+app.use(cors());
+app.use(express.json());
 
 // DB URI
 console.log(process.env.DB_PASS);
@@ -26,14 +32,19 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    
-
-
     app.get("/api/services", async (req, res) => {
       const cursor = serviceCollection.find();
       const result = await cursor.toArray();
       res.send(result);
     });
+    app.get('/api/services/:serviceID', async( req, res) => {
+      const id = req.params.id;
+      // console.log(id);
+      const query = { _id: new ObjectId(id) };
+      const service = await serviceCollection.findOne(query);
+      // console.log(service);
+      res.send(service);
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
@@ -50,8 +61,6 @@ run().catch(console.dir);
 app.get("/", (req, res) => {
   res.send("Handymate is running");
 });
-
-
 
 app.listen(port, () => {
   console.log(`Handymate app listening on port ${port}`);
